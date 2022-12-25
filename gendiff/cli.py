8371f2179e3cc9848eb86579e6d6a89eac1ccf41
Dmitrii Mikailov <dmi.sm.rf@yandex.ru>
2022-12-25 17:25:57 +0300

    feat: add option format

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -6,7 +6,10 @@ def parse_arguments():
         prog='Gendiff',
         description='Compares two configuration files and shows a difference.'
     )
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
+
+    parser.add_argument('first_file', type=str, help='first file')
+    parser.add_argument('second_file', type=str, help='second file')
+    parser.add_argument('-f', '--format', help='set format of output')
+
     args = parser.parse_args()
     return args

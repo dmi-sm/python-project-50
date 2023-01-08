ffc990893519d84b9600fec70163c8a6364c296c
Dmitrii Mikailov <dmi.sm.rf@yandex.ru>
2023-01-08 17:39:37 +0300

    add: test parser

--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -1,15 +1,18 @@
-import pytest
 from gendiff.generate_difference import generate_diff
+from gendiff.parser import parser
 
 
-files = (
-    'gendiff/tests/fixtures/file1.json',
-    'gendiff/tests/fixtures/file2.json'
-)
-result = 'gendiff/tests/fixtures/test'
+first_file = 'gendiff/tests/fixtures/file1.json'
+second_file = 'gendiff/tests/fixtures/file2.json'
+expected = 'gendiff/tests/fixtures/test'
+expected1 = 'gendiff/tests/fixtures/test1'
 
 
-@pytest.mark.parametrize('paths, expected', [(files, result)])
-def test_generate_diff(paths, expected):
+def test_generate_diff():
     with open(expected) as f:
-        assert generate_diff(paths) == f.read()
+        assert generate_diff(first_file, second_file) == f.read()
+
+
+def test_parser():
+    with open(expected1) as f:
+        assert str(parser(first_file)) == f.read()

1c10f8f3f8661632739290fa6a38e211e633e3d4
Dmitrii Mikailov <dmi.sm.rf@yandex.ru>
2023-01-12 22:37:19 +0300

    Update: add new test

--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -1,18 +1,20 @@
 from gendiff.generate_difference import generate_diff
-from gendiff.parser import parser
 
 
-first_file = 'gendiff/tests/fixtures/file1.json'
-second_file = 'gendiff/tests/fixtures/file2.json'
-expected = 'gendiff/tests/fixtures/test'
-expected1 = 'gendiff/tests/fixtures/test1'
+first_plane = 'gendiff/tests/fixtures/plane/file1.json'
+second_plane = 'gendiff/tests/fixtures/plane/file2.json'
+expected_plane = 'gendiff/tests/fixtures/plane/testplane'
+
+first_nested = 'gendiff/tests/fixtures/nested/file1.json'
+second_nested = 'gendiff/tests/fixtures/nested/file2.json'
+expected_nested = 'gendiff/tests/fixtures/nested/testnested'
 
 
-def test_generate_diff():
-    with open(expected) as f:
-        assert generate_diff(first_file, second_file) == f.read()
+def test_plane_generate_diff():
+    with open(expected_plane) as f:
+        assert generate_diff(first_plane, second_plane) == f.read()
 
 
-def test_parser():
-    with open(expected1) as f:
-        assert str(parser(first_file)) == f.read()
+def test_nested_generate_diff():
+    with open(expected_nested) as f:
+        assert generate_diff(first_nested, second_nested) == f.read()

7696df6753caab8600a30af451166e51830ad72f
Dmitrii Mikailov <dmi.sm.rf@yandex.ru>
2023-01-12 22:39:29 +0300

    Refactor: rename vars for better readability

--- gendiff/parse.py
+++ gendiff/parse.py
@@ -2,12 +2,11 @@ import json
 import yaml
 
 
-def parser(file_path):
+def get_content(file_path):
     if file_path.endswith('.json'):
         with open(f'{file_path}') as content:
             return json.load(content)
     elif file_path.endswith('.yml') or file_path.endswith('.yaml'):
         with open(f'{file_path}') as content:
             return yaml.safe_load(content)
-    else:
-        raise Exception('Comparison is available only for json and yaml files')
+    raise Exception('Comparison is available only for json and yaml files')

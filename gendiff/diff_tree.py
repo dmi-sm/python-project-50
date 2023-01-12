8dc0d457b7a4c7fb69a56b0230c21743ed28a1b4
Dmitrii Mikailov <dmi.sm.rf@yandex.ru>
2023-01-12 22:40:35 +0300

    Refactor: change structure of the project

---
+++ gendiff/diff_tree.py
@@ -0,0 +1,48 @@
+def get_diff_tree(first_source, second_source):
+    diff_tree = []
+
+    first_keys = set(first_source)
+    second_keys = set(second_source)
+
+    all_keys = sorted(first_keys | second_keys)
+    added = second_keys - first_keys
+    removed = first_keys - second_keys
+
+    for key in all_keys:
+        if key in added:
+            diff_tree.append({
+                'name': key,
+                'status': 'added',
+                'value': second_source.get(key),
+            })
+        elif key in removed:
+            diff_tree.append({
+                'name': key,
+                'status': 'removed',
+                'value': first_source.get(key),
+
+            })
+        elif first_source.get(key) == second_source.get(key):
+            diff_tree.append({
+                'name': key,
+                'status': 'unmodified',
+                'value': first_source.get(key),
+            })
+        elif isinstance(first_source.get(key), dict) \
+                and isinstance(second_source.get(key), dict):
+            diff_tree.append({
+                'name': key,
+                'status': 'nested',
+                'children': get_diff_tree(first_source.get(key),
+                                          second_source.get(key)
+                                          )
+            })
+        else:
+            diff_tree.append({
+                'name': key,
+                'status': 'modified',
+                'value': first_source.get(key),
+                'new_value': second_source.get(key),
+            })
+
+    return diff_tree
--- gendiff/generate_difference.py
+++ gendiff/generate_difference.py
@@ -1,29 +1,11 @@
-from gendiff.parser import parser
+from gendiff.parse import get_content
+from gendiff.formatters.stylish import get_stylish
+from gendiff.diff_tree import get_diff_tree
 
 
-STATUS = {
-    'in_first': '  - ',
-    'in_second': '  + ',
-    'both': '    '
-}
-
-
-def generate_diff(first, second, format=None):
-    first = dict(sorted((parser(first)).items()))
-    second = dict(sorted((parser(second)).items()))
-    all_keys = sorted(set(first | second))
-    diff = '{\n'
-
-    for key in all_keys:
-        if key in first and key not in second:
-            diff += f'{STATUS["in_first"]}{key}: {first[key]}\n'
-        elif key in second and key not in first:
-            diff += f'{STATUS["in_second"]}{key}: {second[key]}\n'
-        elif first[key] != second[key]:
-            diff += f'{STATUS["in_first"]}{key}: {first[key]}\n'
-            diff += f'{STATUS["in_second"]}{key}: {second[key]}\n'
-        else:
-            diff += f'{STATUS["both"]}{key}: {first[key]}\n'
-    diff += '}'
+def generate_diff(first_file, second_file, format=get_stylish):
+    first = get_content(first_file)
+    second = get_content(second_file)
+    diff = format(get_diff_tree(first, second))
 
     return diff

ab81d6b48d823d595a9aceebef4e38922267e18a
Dmitrii Mikailov <dmi.sm.rf@yandex.ru>
2023-01-15 19:09:31 +0300

    Refactor: change structure tests dir

--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -1,20 +1,20 @@
 from gendiff.generate_difference import generate_diff
 
 
-first_plane = 'gendiff/tests/fixtures/plane/file1.json'
-second_plane = 'gendiff/tests/fixtures/plane/file2.json'
-expected_plane = 'gendiff/tests/fixtures/plane/testplane'
+first_flat_stylish = 'gendiff/tests/fixtures/flat/file1.json'
+second_flat_stylish = 'gendiff/tests/fixtures/flat/file2.json'
+expected_flat_stylish = 'gendiff/tests/fixtures/flat/testflatstylish'
 
-first_nested = 'gendiff/tests/fixtures/nested/file1.json'
-second_nested = 'gendiff/tests/fixtures/nested/file2.json'
-expected_nested = 'gendiff/tests/fixtures/nested/testnested'
-
-
-def test_plane_generate_diff():
-    with open(expected_plane) as f:
-        assert generate_diff(first_plane, second_plane) == f.read()
+first_nested_stylish = 'gendiff/tests/fixtures/nested/file1.json'
+second_nested_stylish = 'gendiff/tests/fixtures/nested/file2.json'
+expected_nested_stylish = 'gendiff/tests/fixtures/nested/testnestedstylish'
 
 
-def test_nested_generate_diff():
-    with open(expected_nested) as f:
-        assert generate_diff(first_nested, second_nested) == f.read()
+def test_flat_stylish_generate_diff():
+    with open(expected_flat_stylish) as f:
+        assert generate_diff(first_flat_stylish, second_flat_stylish) == f.read()
+
+
+def test_nested_stylish_generate_diff():
+    with open(expected_nested_stylish) as f:
+        assert generate_diff(first_nested_stylish, second_nested_stylish) == f.read()

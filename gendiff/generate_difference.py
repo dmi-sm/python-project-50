d8ed012e225b7a0a98095f13f6ec04141156ac4c
Dmitrii Mikailov <dmi.sm.rf@yandex.ru>
2023-01-16 23:10:55 +0300

    Fix: set format default for package

--- gendiff/generate_difference.py
+++ gendiff/generate_difference.py
@@ -3,7 +3,7 @@ from gendiff.diff_tree import get_diff_tree
 from gendiff.formatters.formatter import set_format
 
 
-def generate_diff(first_file, second_file, format):
+def generate_diff(first_file, second_file, format='stylish'):
     first = get_content(first_file)
     second = get_content(second_file)
 
